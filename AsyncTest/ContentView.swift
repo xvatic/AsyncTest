3db2e4244210e072145d5957e2be253b74b9ecf6
xvatic <glazkovE@gmail.com>
2020-07-31 23:53:37 +0300

    Initial Commit

---
+++ AsyncTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AsyncTest
+//
+//  Created by Евгений on 7/31/20.
+//  Copyright © 2020 Евгений. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
